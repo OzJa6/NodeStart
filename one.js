d232897f50e2373abbc83be16f8b648dd7110a3a
OzJa6 <mihail-romashov01@yandex.ru>
2022-06-24 09:30:31 +0500

    обработка форм

--- one.js
+++ one.js
@@ -4,6 +4,7 @@ const fortune = require('./lib/fortune')
 const bodyParser = require('body-parser')
 const handlers = require('./lib/handlers')
 const weatherMiddleware = require('./lib/middleware/weather')
+const multiparty = require('multiparty')
 
 const app = express()
 
@@ -34,7 +35,22 @@ app.get('/headers', handlers.headers)
 app.get('/newsletter', handlers.newsletter)
 app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
 
+app.get('/contest/vacation-photo', handlers.vacationPhoto)
+app.post('/api/vacation-photo-contest/:year/:month', (req, res) => {
+    const form = new multiparty.Form()
+    form.parse(req, (err, fields, files) => {
+      if(err) return handlers.ServerError(req, res, err.message)
+      handlers.api.vacationPhotoContest(req, res, fields, files)
+    })
+})
 
+// app.post('/contest/vacation-photo/:year/:month', (req, res) => {
+//     const from = new multiparty.Form()
+//     from.parse(req, (err, fields, files) => {
+//         if(err) return handlers.ServerError
+//         handlers.vacationPhotoContestProcess(req, res, fields, files)
+//     })
+// })
 
 
 app.use(handlers.NotFound)
