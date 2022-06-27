89b5c10c8fc730c9c2e87fe8bff5245d72453b38
OzJa6 <mihail-romashov01@yandex.ru>
2022-06-27 15:09:48 +0500

    welcome to middleware

--- one.js
+++ one.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
-const fortune = require('./lib/fortune')
 const bodyParser = require('body-parser')
 const handlers = require('./lib/handlers')
 const weatherMiddleware = require('./lib/middleware/weather')
 const multiparty = require('multiparty')
+const credentials = require('./credentials')
+const cookieParser = require('cookie-parser')
+const expressSession = require('express-session')
+const flashMiddleware = require('./lib/middleware/flash')
 
 const app = express()
 
@@ -25,6 +28,13 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'))
+app.use(cookieParser(credentials.cookieSecret))
+app.use(expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: credentials.cookieSecret,
+}))
+app.use(flashMiddleware)
 
 app.get('/', weatherMiddleware, handlers.home)
 
@@ -32,8 +42,9 @@ app.get('/about', handlers.about)
 
 app.get('/headers', handlers.headers)
 
-app.get('/newsletter', handlers.newsletter)
-app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
+app.get('/newsletter-signup', handlers.newsletterSignup)
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
+
 
 app.get('/contest/vacation-photo', handlers.vacationPhoto)
 app.post('/api/vacation-photo-contest/:year/:month', (req, res) => {
@@ -44,17 +55,7 @@ app.post('/api/vacation-photo-contest/:year/:month', (req, res) => {
     })
 })
 
-// app.post('/contest/vacation-photo/:year/:month', (req, res) => {
-//     const from = new multiparty.Form()
-//     from.parse(req, (err, fields, files) => {
-//         if(err) return handlers.ServerError
-//         handlers.vacationPhotoContestProcess(req, res, fields, files)
-//     })
-// })
-
-
 app.use(handlers.NotFound)
-
 app.use(handlers.ServerError)
 
 app.listen(port, () => console.log('express started' + `on ${port}` + 'press ctrl+c'))
